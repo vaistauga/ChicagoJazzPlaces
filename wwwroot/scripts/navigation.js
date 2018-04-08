af740a4921a19f9a63158444fafd1f946ea23312
Vaidotas Staugaitis <vstaugaitis@sanityassembly.com>
2018-04-07 20:20:14 -0500

    The navigation toggles according to media query

--- wwwroot/scripts/navigation.js
+++ wwwroot/scripts/navigation.js
@@ -5,3 +5,18 @@ $(function() {
     console.log("menu was toogled");
   });
 });
+
+$(function() {
+  $(window).resize(function() {
+    if (
+      window.innerWidth > 700 &&
+      !$(".toggle-menu-button").hasClass("active")
+    ) {
+      $(".toggle-menu-button").addClass("active");
+      $(".menu-drawer").addClass("open");
+    } else if (window.innerWidth <= 700) {
+      $(".toggle-menu-button").removeClass("active");
+      $(".menu-drawer").removeClass("open");
+    }
+  });
+});
