aff22a2ef78c5b19dc4475cc4a78f968ef239c87
Vai <vaidorass@live.com>
2018-04-08 19:59:46 -0500

    added more jazzy places

--- model/places.js
+++ model/places.js
@@ -2,9 +2,9 @@
 //Essentialy this is my database, If this were a real application i would move it into an actual database.
 var places = [
   {
-    title: "Navy Peer",
-    position: { lat: 41.891551, lng: -87.607375 },
-    fourSquareId: "45840abff964a520913f1fe3"
+    title: "Bandera Restaurant",
+    position: { lat:41.892014 , lng:-87.623597 },
+    fourSquareId: "429e4c00f964a520b7241fe3"
   },
 
   {
@@ -13,7 +13,18 @@ var places = [
     fourSquareId: "40b28c80f964a520d9fb1ee3"
   },
   {
-    title: "Museum of Science & Industry",
-    position: { lat: 41.791605, lng: -87.5837228 },
-    fourSquareId: "42eeb780f964a520b4261fe3"
-  }]
+    title: "Kingston Mines",
+    position: { lat: 41.928718, lng: -87.649211 },
+    fourSquareId: "40b28c80f964a52040fc1ee3"
+  },
+  {
+    title: "Green Mill Cocktail Lounge",
+    position: { lat:41.969209 , lng: -87.659904 },
+    fourSquareId: "40b28c80f964a52071fe1ee3"
+  },
+  {
+    title: "Jazz Showcase",
+    position: { lat:41.871799 , lng:  -87.628843},
+    fourSquareId: "4b699582f964a52061a82be3"
+  }
+]
